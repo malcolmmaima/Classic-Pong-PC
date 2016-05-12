75bf1e78b25c1a2f203fdcd0ea124f05c3e65cdf
malcolm maima <malcolmmaima@gmail.com>
2016-05-12 18:07:20 +0300

    Classic Pong game | PC
    This is a simple pong classic pong game

---
+++ Assets/MoveRacket.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRacket : MonoBehaviour {
+
+    public float speed = 30;
+    public string axis = "Vertical";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate() {
+        float v = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+    }
+}
